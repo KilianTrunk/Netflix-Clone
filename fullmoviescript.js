d9b960096446fd93814b529b9befec86ccf0f4a9
KilianTrunk <hunterteammovies@gmail.com>
2021-07-17 16:13:36 +0200

    updated files

--- fullmoviescript.js
+++ fullmoviescript.js
@@ -1,34 +1,126 @@
-const API_URL = 'https://api.themoviedb.org/3/movie/550?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+var API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+var currentPageUrl = new URL(window.location.href);
 
 async function getMovieData() {
+
   const response = await fetch(API_URL);
   const json = await response.json();
 
-  // fullmovie.html START
+  let params = currentPageUrl.searchParams;
+  var movieID = params.get('movieid');
 
-  const movieTitle = document.querySelector('#movieTitle');
-  movieTitle.innerHTML = json.original_title;
+  var test = document.getElementById("movieTitle");
 
-  const movieOverview = document.querySelector('#movieOverview');
-  movieOverview.innerHTML = json.overview;
+  /* if (movieID === undefined) {
+    API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+    const response = await fetch(API_URL);
+    const json = await response.json();
 
-  const movieReleaseDate = document.querySelector('#movieReleaseDate');
-  movieReleaseDate.innerHTML = json.release_date;
+    console.log(json);
 
-  const movieLanguage = document.querySelector('#movieLanguage');
-  movieLanguage.innerHTML = json.original_language;
+    const movieTitle = document.querySelector('#movieTitle');
+    movieTitle.innerHTML = json.results[0].original_title;
 
-  const movieTagline = document.querySelector('#movieTagline');
-  movieTagline.innerHTML = json.tagline;
+    const movieOverview = document.querySelector('#movieOverview');
+    movieOverview.innerHTML = json.results[0].overview;
 
-  const movieHomepage = document.querySelector('#movieHomepage');
-  movieHomepage.innerHTML = json.homepage;
+    const movieReleaseDate = document.querySelector('#movieReleaseDate');
+    movieReleaseDate.innerHTML = json.results[0].release_date;
 
-  document.getElementById("details").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/"+json.backdrop_path+"')";
+    const movieLanguage = document.querySelector('#movieLanguage');
+    movieLanguage.innerHTML = json.results[0].original_language;
 
-  // fullmovie.html END
+    const movieTagline = document.querySelector('#movieTagline');
+    movieTagline.innerHTML = json.results[0].tagline;
 
-  console.log(json);
+    const movieHomepage = document.querySelector('#movieHomepage');
+    movieHomepage.innerHTML = json.results[0].homepage;
+
+    document.getElementById("details").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/" + json.results[0].backdrop_path + "')";
+  } else {
+    API_URL = 'https://api.themoviedb.org/3/movie/' + movieID + '?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+    const response = await fetch(API_URL);
+    const json = await response.json();
+
+    console.log(json);
+
+    const movieTitle = document.querySelector('#movieTitle');
+    movieTitle.innerHTML = json.original_title;
+
+    const movieOverview = document.querySelector('#movieOverview');
+    movieOverview.innerHTML = json.overview;
+
+    const movieReleaseDate = document.querySelector('#movieReleaseDate');
+    movieReleaseDate.innerHTML = json.release_date;
+
+    const movieLanguage = document.querySelector('#movieLanguage');
+    movieLanguage.innerHTML = json.original_language;
+
+    const movieTagline = document.querySelector('#movieTagline');
+    movieTagline.innerHTML = json.tagline;
+
+    const movieHomepage = document.querySelector('#movieHomepage');
+    movieHomepage.innerHTML = json.homepage;
+
+    document.getElementById("details").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/" + json.backdrop_path + "')";
+  } */
+
+  var test1 = json.status_code;
+
+  if (test1 !== undefined) {
+    API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+    const response = await fetch(API_URL);
+    const json = await response.json();
+
+    console.log(json);
+
+    const movieTitle = document.querySelector('#movieTitle');
+    movieTitle.innerHTML = json.results[0].original_title;
+
+    const movieOverview = document.querySelector('#movieOverview');
+    movieOverview.innerHTML = json.results[0].overview;
+
+    const movieReleaseDate = document.querySelector('#movieReleaseDate');
+    movieReleaseDate.innerHTML = json.results[0].release_date;
+
+    const movieLanguage = document.querySelector('#movieLanguage');
+    movieLanguage.innerHTML = json.results[0].original_language;
+
+    const movieTagline = document.querySelector('#movieTagline');
+    movieTagline.innerHTML = json.results[0].tagline;
+
+    const movieHomepage = document.querySelector('#movieHomepage');
+    movieHomepage.innerHTML = json.results[0].homepage;
+
+    document.getElementById("details").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/" + json.results[0].backdrop_path + "')";
+  } else {
+    API_URL = 'https://api.themoviedb.org/3/movie/' + movieID + '?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+    const response = await fetch(API_URL);
+    const json = await response.json();
+
+    console.log(json);
+
+    const movieTitle = document.querySelector('#movieTitle');
+    movieTitle.innerHTML = json.original_title;
+
+    const movieOverview = document.querySelector('#movieOverview');
+    movieOverview.innerHTML = json.overview;
+
+    const movieReleaseDate = document.querySelector('#movieReleaseDate');
+    movieReleaseDate.innerHTML = json.release_date;
+
+    const movieLanguage = document.querySelector('#movieLanguage');
+    movieLanguage.innerHTML = json.original_language;
+
+    const movieTagline = document.querySelector('#movieTagline');
+    movieTagline.innerHTML = json.tagline;
+
+    const movieHomepage = document.querySelector('#movieHomepage');
+    movieHomepage.innerHTML = json.homepage;
+
+    document.getElementById("details").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/" + json.backdrop_path + "')";
+  }
+
 }
 
 getMovieData();
--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,30 @@
-const API_URL = 'https://api.themoviedb.org/3/movie/550?api_key=b0d1306fad90411efb79cc7bced5c6f2';
+var API_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=b0d1306fad90411efb79cc7bced5c6f2';
 
 async function getMovieData() {
   const response = await fetch(API_URL);
   const json = await response.json();
 
-  // index.html START
+  var movieID = json.results[0].id;
 
   const title = document.querySelector('#originalTitle');
-  title.innerHTML = json.original_title;
+  title.innerHTML = json.results[0].original_title;
 
   const overview = document.querySelector('#overviewText');
-  overview.innerHTML = json.overview;
+  overview.innerHTML = json.results[0].overview;
 
-  document.getElementById("main-part").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/"+json.backdrop_path+"')";
+  document.getElementById("main-part").style.backgroundImage = "url('https://image.tmdb.org/t/p/original/"+json.results[0].backdrop_path+"')";
 
-  // index.html END
+  document.getElementById("fullMovieButton").href="fullmovie.html?movieid="+movieID+" ";
+
+  document.getElementById("img1").src = "https://image.tmdb.org/t/p/original/"+json.results[1].backdrop_path+" ";
+  document.getElementById("img2").src = "https://image.tmdb.org/t/p/original/"+json.results[2].backdrop_path+" ";
+  document.getElementById("img3").src = "https://image.tmdb.org/t/p/original/"+json.results[3].backdrop_path+" ";
+  document.getElementById("img4").src = "https://image.tmdb.org/t/p/original/"+json.results[4].backdrop_path+" ";
+  document.getElementById("img5").src = "https://image.tmdb.org/t/p/original/"+json.results[5].backdrop_path+" ";
+  document.getElementById("img6").src = "https://image.tmdb.org/t/p/original/"+json.results[6].backdrop_path+" ";
+  document.getElementById("img7").src = "https://image.tmdb.org/t/p/original/"+json.results[7].backdrop_path+" ";
+  document.getElementById("img8").src = "https://image.tmdb.org/t/p/original/"+json.results[8].backdrop_path+" ";
+  document.getElementById("img9").src = "https://image.tmdb.org/t/p/original/"+json.results[9].backdrop_path+" ";
 
   console.log(json);
 }
